9acbfb6daf10e631c1e66d5c2154e5ee1fd0f5be
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-07-26 11:33:15 +0000

    add Eggcartons
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@781 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/week01practice/EggCartons.hpp
@@ -0,0 +1,51 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  EggCartons.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/25/2017 04:09:19 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class EggCartons{
+	
+	public:
+		int minCartons(int n){
+			int res;
+			bool check = true;
+			while(check){
+				if(n<0){
+					check = false;
+					res = -1;
+				}else if(n%8==0){
+					res += n/8;
+					check = false;
+				}else{
+					n -= 6;
+					res++;				
+				}	
+			}
+			return res;
+		}
+
+};
+/*
+int main(){
+    EggCartons test;
+    cout << test.minCartons(4) << endl;
+
+}
+*/
