c7b6bfa9a72ab5b5b08922b1df3ca00a364b1cea
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-01 07:07:34 +0000

    testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@818 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/diagp2/SlowKeyboard.hpp
+++ pssd/diagp2/SlowKeyboard.hpp
@@ -41,14 +41,15 @@ public:
 
 			if(minD==5||minR==secondD){
 				if(secondD==0){
-					if((10-minR)>=minimum){
-						if(diff*2< 10-minR){
-							return minimum + diff*2;
-						}else{
-							return minimum + (10-minR) ;
-						}
+					if((10-minR)==minimum){
+						return minimum;
 					}else{
-						return minimum + 2;
+						if(diff*2< 10-minR){
+                             return minimum + diff*2;
+                          }else{           
+                              return minimum + (10-minR) ;
+                          } 
+
 					}
 				}				
 			}
