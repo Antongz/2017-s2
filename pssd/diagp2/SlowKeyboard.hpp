34886e2e273105888e78fcfd12677597c7115e84
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-01 06:15:34 +0000

    testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@801 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/diagp2/SlowKeyboard.hpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string.h>
+#include <cstring>
+
+using namespace std;
+
+class SlowKeyboard{
+
+
+public:
+	int enterTime(string time){
+		int res = 0;
+		int diff = 0;
+		int min = 4;
+		char prev = time[0];
+		for(int i=1;i<time.length();i++){
+			if(prev!=time[i])
+				diff++;
+
+			prev = time[i]
+		}
+
+		const char* sym[2] = ":";
+		char* sec = new char[time.length()+1];
+		strcpy(sec,time.c_str());
+		char* min = strtok(sec,sym);
+		sec = strtok(NULL,sym);
+		
+		int miniute = atoi(min);
+		int second = atoi(sec);
+
+		
+
+
+
+	
+
+		return min + diff*2;
+
+
+
+
+
+		
+	}
+
+
+};
