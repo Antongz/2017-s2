57cebe101139e2b8af3f5ab553b1a8953dbf9ae7
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-01 06:33:35 +0000

    testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@807 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/diagp3/SumOfPower.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+
+using namespace std;
+
+class SumOfPower{
+
+
+	public:
+		int findSum(vector<int> array){
+			int sum = 0;
+			int subsum = 0;
+			
+			for(int i=0;i<array.size();i++){
+				subsum = 0;
+				for(int j=i+1;j<array.size();j++){
+					subsum += array[j];
+				}
+				sum += subsum;
+			}
+
+
+			return sum;
+		}
+
+
+};
