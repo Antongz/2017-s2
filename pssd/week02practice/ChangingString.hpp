dec08e6c8943da8a8c48e20d38a130457d62171c
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-04 12:33:48 +0000

    upload for testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@823 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week02practice/ChangingString.hpp
+++ pssd/week02practice/ChangingString.hpp
@@ -25,42 +25,43 @@ using namespace std;
 
 class ChangingString{
 
-	public:
-		int distance(string A,string B,int k){
-			int res = 0;
-			vector<int> diff;
-			for(int i=0;i<A.length();i++){
-				char a = A[i];
-				char b = B[i];
-				diff.push_back(abs(a-b));				
-			}
+    public:
+        int distance(string A,string B,int k){
+            int res = 0;
+            vector<int> diff;
+            for(int i=0;i<A.length();i++){
+                char a = A[i];
+                char b = B[i];
+                diff.push_back(abs(a-b));
+            }
 
-			int temp = 0;
-			//sort our diff list
-			for(int i=0;i<diff.size();i++){
-				for(int j=0;j<diff.size();j++){
-					if(diff[j]>diff[i]){
-						temp = diff[j];
-						diff[j] = diff[i];
-						diff[i] = temp;
-					}
-				}
-			}
+            int temp = 0;
+            //sort our diff list
+            for(int i=0;i<diff.size();i++){
+                for(int j=i+1;j<diff.size();j++){
+                    if(diff[j]>diff[i]){
+                        temp = diff[j];
+                        diff[j] = diff[i];
+                        diff[i] = temp;
+                    }
+                }
+            }
 
-			//change k character
-			for(int i=0;i<diff.size();i++){
-				if(k>0){
-					if(diff[i]==0){
-						diff[i] = 1;
-					}else{
-						diff[i] = 0;
-					}
-					k--;
-				}
+            //change k character
+            for(int i=0;i<diff.size();i++){
+                if(k>0){
+                    if(diff[i]==0){
+                        diff[i] = 1;
+                    }else{
+                        diff[i] = 0;
+                    }
+                    k--;
+                }
 
-				res += diff[i];
-			}
-			return res;
-		}
+                res += diff[i];
+            }
+            return res;
+        }
 
 };
+
