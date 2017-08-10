ed4925596ea5abd336110696fa51e05fa30cdfea
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-10 13:05:30 +0000

    i think i finish alternate
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@846 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week02practice/AlternateColors.hpp
+++ pssd/week02practice/AlternateColors.hpp
@@ -21,116 +21,97 @@
 
 using namespace std;
 
-class AlternateColors{
+class ChristmasTree{
     public:
-        string getColor(long r,long g,long b,long k){
-
-            int color = currColor(r,g,b,k,0);
-            if(color==1)
-                return "RED";
-            if(color==2)
-                return "GREEN";
-            if(color==3)
-                return "BLUE";
-        }
-
-        int currColor(long r,long g,long b,long k,int help){
-            long min = 0;
-            if(r<=g&&r<=b){
-                min = r;
-                help = 1;
+        long decorationWays(int N,int red,int green,int blue){
+            int sum = 0;
+            for(int i=1;i<=N;i++){
+                sum += i;
             }
-            if(g<=b&&g<=r){
-                min = g;
-                help = 2;
-            }
-
-            if(b<=r&&b<=g){
-                min = b;
-                help = 3;
-            }
-
-            if(k<=3*(min-1)+help){
-                if(k%3==1){
-                    return 1;
-                }
-                if(k%3==2){
-                    return 2;
-                }
-                if(k%3==0){
-                    return 3;
-                }
+            if(red+green+blue<sum||red<0||green<0||blue<0){
+                //cout << "0 here" << endl;
+                return 0;
             }else{
-                if(r==min){
-                    if(g-min==0){
-                        return 3;
-                    }if(b-min==0){
-                        return 2;
-                    }return currColor2(g-min,b-min,k-3*(min-1)-1,help);
-                }
-                if(g==min){
-                    if(r-min==0){
-                        return 3;
-                    }if(b-min==0){
-                        return 1;
-                    }return currColor2(b-min,r-min,k-3*(min-1)-2,help);
-                }
-                if(b==min){
-                    if(r-min==0){
-                        return 2;
-                    }if(g-min==0){
-                        return 1;
-                    }return currColor2(r-min,g-min,k-3*(min-1)-3,help);
-                }
-            }
-        }
-        int currColor2(long a,long b,long k,int help){
-            long min = 0;
-            if(a<b){
-                min = a;
-            }else{
-                min = b;
-            }
-            if(k<=2*min){
-                if(k%2==1){
-                    if(help==1)
-                        return 2;
-                    if(help==2)
-                        return 3;
-                    if(help==3)
-                        return 1;
+                if(N==1&&red>=0&&green>=0&&blue>=0){
+                    long res = 0;
+                    if(red>0){
+                        //cout << "red ";
+                        res++;
+                    }
+                    if(green>0){
+                       // cout << "green ";
+                        res++;
+                    }
+                    if(blue>0){
+                       // cout << "blue ";
+                        res++;
+                    }
+                   // cout << "return res: " << res << endl;
+                    return res;
                 }else{
-                    if(help==1)
-                        return 3;
-                    if(help==2)
-                        return 1;
-                    if(help==3)
-                        return 2;
-                }
-            }else{
-                if(help==1){
-                    if(a<b){
-                        return 3;
-                    }else{
-                        return 2;
+                    if(N%3==0&&N%2!=0){
+                        int mul3 = 1;
+                        int div3 = 1;
+                        for(int i=N/3+1;i<=N;i++){
+                            mul3 = mul3*(i);
+                        }
+                        for(int i=1;i<=2*N/3;i++){
+                            div3 = div3*i;
+                        }//cout<< "mul3/div3: " << mul3/div3 << endl;;
+
+                        int mul2 = 1;
+                        int div2 = 1;
+                        for(int i=N/3+1;i<=2*N/3;i++){
+                            mul2 = mul2*(i);
+                            div2 = div2*(i-N/3);
+                        }
+                        //cout << "mul2/div2: " << mul2/div2 << endl;
+
+                        return decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N)+(mul3/div3)*(mul2/div2)*decorationWays(N-1,red-N/3,green-N/3,blue-N/3);
                     }
-                }
-                if(help==2){
-                    if(a<b){
-                        return 1;
-                    }else{
-                        return 3;
+                    else if(N%2==0&&N%3!=0){
+                        int mul = 1;
+                        int div = 1;
+                        for(int i=N/2+1;i<=N;i++){
+                            mul = mul*(i);
+                            div = div*(i-N/2);
+                        }
+                        return (mul/div)*decorationWays(N-1,red-N/2,green-N/2,blue)+(mul/div)*decorationWays(N-1,red-N/2,green,blue-N/2)+(mul/div)*decorationWays(N-1,red,green-N/2,blue-N/2)
+                                +decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N);
                     }
-                }
-                if(help==3){
-                    if(a<b){
-                        return 2;
-                    }else{
-                        return 1;
+                    else if(N%2==0&&N%3==0){
+                        int mul3 = 1;
+                        int div3 = 1;
+                        for(int i=N/3+1;i<=N;i++){
+                            mul3 = mul3*(i);
+                        }
+                        for(int i=1;i<=2*N/3;i++){
+                            div3 = div3*i;
+                        }//cout<< "mul3/div3: " << mul3/div3 << endl;;
+
+                        int mul2 = 1;
+                        int div2 = 1;
+                        for(int i=N/3+1;i<=2*N/3;i++){
+                            mul2 = mul2*(i);
+                            div2 = div2*(i-N/3);
+                        }
+
+                        int mul = 1;
+                        int div = 1;
+                        for(int i=N/2+1;i<=N;i++){
+                            mul = mul*(i);
+                            div = div*(i-N/2);
+                        }
+
+                        return decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N)+(mul3/div3)*(mul2/div2)*decorationWays(N-1,red-N/3,green-N/3,blue-N/3)+
+                                (mul/div)*decorationWays(N-1,red-N/2,green-N/2,blue)+(mul/div)*decorationWays(N-1,red-N/2,green,blue-N/2)+(mul/div)*decorationWays(N-1,red,green-N/2,blue-N/2);
+                    }
+
+                    else{
+                        return decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N);
                     }
                 }
             }
         }
-
 
 };
