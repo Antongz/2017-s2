06cebb29b9bc11ca0cba5b439fb96057e1aa82e7
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-10 11:17:18 +0000

    update ChristmasTree
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@842 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week02practice/ChristmasTree.hpp
+++ pssd/week02practice/ChristmasTree.hpp
@@ -24,49 +24,36 @@
 using namespace std;
 
 class ChristmasTree{
-	public:
-		long decorationWays(int N,int red,int green,int blue){
+    public:
+        long decorationWays(int N,int red,int green,int blue){
             int sum = 0;
-			for(int i=1;i<=N;i++){
-				sum += i;
-			}
-			if(red+green+blue<sum){
-				return 0;
+            for(int i=1;i<=N;i++){
+                sum += i;
+            }
+            if(red+green+blue<sum||red<0||green<0||blue<0){
+                return 0;
             }else{
-                return count(red,green,blue,N); 
-			}
-		}
+                if(N==1){
+                    long res = 0;
+                    if(red>0)
+                        res++;
+                    if(green>0)
+                        res++;
+                    if(blue>0)
+                        res++;
 
-		long count(int red,int green,int blue,int layer){
-            long res = 0;
-			
-			if(red+green+blue<layer||red<0||green<0||blue<0)
-				return 0;
-			if(layer==1){
-				if(red>0){
-					res++;
-                }
-				if(green>0){
-					res++;
-                }
-				if(blue>0){
-					res++;
-                }
-                return res;
-            }else{
-				//when the layer is not equay to the first one
-                if(layer%3==0){
-                //this could have 3! choices
-                        return count(red-layer,green,blue,layer--)+count(red,green-layer,blue,layer--)+count(red,green,blue-layer,layer--)+ count(red-layer/3,green-layer/3,blue-layer/3,layer--);
-                }
-                if(layer%2==0){
-                        return count(red-layer/2,green-layer/2,blue,layer--)+count(red-layer/2,green,blue-layer/2,layer--)+count(red,green-layer/2,blue-layer/2,layer--);
-                }
-                return count(red-layer,green,blue,layer--)+count(red,green-layer,blue,layer--)+count(red,green,blue-layer,layer--);
+                    return res;
+                }else{
+                    if(N%3==0&&N%2!=0){
+                        return decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N)+decorationWays(N-1,red-N/3,green-N/3,blue-N/3);
+                    }
+                    if(N%2==0&&N%3!=0){
+                        return decorationWays(N-1,red-N/2,green-N/2,blue)+decorationWays(N-1,red-N/2,green,blue-N/2)+decorationWays(N-1,red,green-N/2,blue-N/2)+decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N);
+                    }
 
-			}
-
-		}
-	
+                    return decorationWays(N-1,red-N,green,blue)+decorationWays(N-1,red,green-N,blue)+decorationWays(N-1,red,green,blue-N);
+                }
+            }
+        }
 
 };
