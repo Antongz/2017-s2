6811bea4c22002ab7e4a6061a59a09c0692e34db
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-15 04:46:25 +0000

    upload truckloads and fraccount to test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@856 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/week03practice/FracCount.hpp
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  FracCount.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/12/2017 06:00:16 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+
+using namespace std;
+
+class FracCount{
+
+
+	public:
+		int position(int numerator,int denominator){
+            if(numerator==1&&denominator==2)
+                return 1;
+            if(numerator==1)
+                return 1+position(denominator-2,denominator--);
+            else{
+                if(checkGCD(numerator,denominator))
+                    return position(numerator-1,denominator);
+                else
+                    return 1+position(numerator-1,denominator);
+            }
+
+		}
+        bool checkGCD(int numerator,int denominator){
+            int tmp = numerator;
+            int c = denominator % numerator;
+            if(c==0){
+                return true;
+            }
+            if(denominator<numerator){
+                return false;
+            }
+            else{
+                return checkGCD(numerator,c);
+            }
+			
+		}
+
+};
---
+++ pssd/week03practice/Truckloads.hpp
@@ -0,0 +1,34 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Truckloads.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  08/12/2017 05:50:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Truckloads{
+
+	public:
+		int numTrucks(int numCrates,int loadSize){
+			if(numCrates<=loadSize)
+				return 1;			
+			else
+				return 2*numTrucks(numCrates/2,loadSize);		
+		}
+
+
+};
