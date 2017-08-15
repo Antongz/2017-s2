968a4d5b494e0efa9bde05b3c6a66157d57d1eaf
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-15 06:13:36 +0000

    test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@864 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/pracexam1p1/GoingUp.hpp
+++ pssd/pracexam1p1/GoingUp.hpp
@@ -19,13 +19,24 @@ class GoingUp{
 
 			else{
 				if(b<=a)
-					return a-b+1+minRemove(a-b-1,b,c);
+					return a-b+1+minRemove(a-(a-b+1),b,c);
 				if(c<=b)
-					return b-c+1+minRemove(a,b-c-1,c);
+					return b-c+1+minRemove(a,b-(b-c+1),c);
 				if(c<=a)
-					return a-c+1+minRemove(a-c-1,b,c); 
+					return a-c+1+minRemove(a-(a-c+1)-1,b,c); 
 			}
 		}
 		
 
 };
+
+
+int main(){
+	GoingUp test;
+	int a,b,c;
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cout << test.minRemove(a,b,c) << endl;
+
+}
