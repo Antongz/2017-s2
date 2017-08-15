acd219616b28c93967e9bdacfca40e57d5163435
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-15 07:13:30 +0000

    test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@869 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/pracexam1p2/Teleport.hpp
+++ pssd/pracexam1p2/Teleport.hpp
@@ -16,23 +16,74 @@ class Teleport{
 		int Xdiff = abs(xHome-xMe);
 		int Ydiff = abs(yHome-yMe);
 		const char space [2] = " ";
+		string a,b,c,d;
 		vector<int> stX;
 		vector<int> stY;
 		vector<int> resX;
 		vector<int> resY;
 		int xPob = 0;	int yPob = 0;
 		for(int i=0;i<test.size();i++){
-			//stX.push_back(stoi(strtok(test[i],space)));
-			//stY.push_back(stoi(strtok(NULL,space)));
-			//resX.push_back(stoi(strtok(NULL,space)));
-			//resY.push_back(stoi(strtok(NULL,space)));
+			for(int i=0;i<4;i++){
+			 	a=strtok(test[i],space);
+				b=strtok(NULL,space);
+				c=strtok(NULL,space);
+				d=strtok(NULL,space);
+				stX.push_back(stoi(a));
+				stY.push_back(stoi(b));
+				resX.push_back(stoi(c));
+				resY.push_back(stoi(d));
+			}
+		bool check = false;}
+		int minDiff = Xdiff+Ydiff;
+		int count = 0;
+		int Begin = 0;
+		for(int i=0;i<stX.size();i++){
+			if(abs(stX[i]-xMe)+abs(stY[i]-yMe)<minDiff){
+				minDiff = abs(stX[i]-xMe)+abs(stY[i]-yMe);
+				count = i;
+				Begin = 0;
+				check = true;
+			}
+			if(abs(resX[i]-xMe)+abs(resY[i]-yMe)<minDiff){
+				minDiff = abs(resX[i]-xMe)+abs(resY[i]-yMe);
+				count = i;
+				Begin = 1;
+				check = true;
+			}
 		}
+
+		int compare = 0;
+		if(check){
+			if(Begin==0){
+				compare = abs(stX[count]-xMe)+abs(stY[count]-yMe) + 10 + abs(resX[count]-xHome)+abs(resY[count]-yHome);
+			}
+			if(Begin==1){                                                                           
+                  compare = abs(stX[count]-xHome)+abs(stY[count]-yHome) + 10 + abs(resX[count]-xMe)+abs(resY[count]-yMe);                                                                                           }  
+		
+		}
+		
+
 
 		int chose = 0;
 		
+		if(compare<=Xdiff+Ydiff)
+			return compare;
 
 		return Xdiff+Ydiff;
 	}
 
 
 };
+
+/*
+int main(){
+	Teleport test;
+	vector<string> t;
+	t.push_back("1000 1001 1000 1002");
+	t.push_back("1000 1003 1000 1004");
+	t.push_back("1000 1005 1000 1006");
+	test.bestPath(3,3,4,5,t);
+	
+	return 0;
+}
+*/
