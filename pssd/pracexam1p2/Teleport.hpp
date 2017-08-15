0a94ea674216930b3aea681e4b38fb4311255342
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-15 06:34:30 +0000

    firstTest
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@866 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/pracexam1p2/Teleport.hpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+class Teleport{
+	int bestPath(int xMe,int yMe,int xHome,int yHome,vector<string> test){
+		int Xdiff = abs(xHome-xMe);
+		int Ydiff = abs(yHome-yMe);
+		const char space [2] = " ";
+		vector<int> stX;
+		vector<int> stY;
+		vector<int> resX;
+		vector<int> resY;
+		int xPob = 0;	int yPob = 0;
+		for(int i=0;i<test.size();i++){
+			stX.push_back(stoi(strtok(test[i],space)));
+			stY.push_back(stoi(strtok(NULL,space)));
+			resX.push_back(stoi(strtok(NULL,space)));
+			resY.push_back(stoi(strtok(NULL,space)));
+		}
+
+		int chose = 0;
+		
+
+		return Xdiff+Ydiff;
+	}
+
+
+};
