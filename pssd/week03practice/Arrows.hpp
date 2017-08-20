9e6fce427bff84b8f08cd469770df2bdf44c817c
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-08-20 09:01:21 +0000

    test arrow
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@882 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week03practice/Arrows.hpp
+++ pssd/week03practice/Arrows.hpp
@@ -29,11 +29,13 @@ class Arrows{
             int itr = 0;
             int tempM = 0;
             char check;
+			bool test = false;
             for(int i=0;i<s.length();i++){
                 itr = i;
                 if(s[i]=='<'){
                     int temp = 0;
                     check = s[i+1];
+					test = true;
                     if(check!=s[i]&&check!='>'){
                         temp++;
                         itr++;
@@ -48,6 +50,7 @@ class Arrows{
                 if(s[i]=='>'){
                     int temp = 0;
                     check = s[i-1];
+					test = true;
                     if(check!=s[i]&&check!='<'){
                         temp++;
                         itr--;
@@ -60,6 +63,8 @@ class Arrows{
                         tempM = temp;
                 }
             }
+			if(test&&temp==0)
+				return 1;
             if(tempM==0)
                 return -1;
 
