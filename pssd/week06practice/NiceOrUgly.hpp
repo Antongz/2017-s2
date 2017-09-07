487f9f5e4753d110395966270ef9b7f63e58516a
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-07 13:31:56 +0000

    try niceorUgly again
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@917 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week06practice/NiceOrUgly.hpp
+++ pssd/week06practice/NiceOrUgly.hpp
@@ -31,40 +31,52 @@ class NiceOrUgly{
     }
 
     string check(string s,int itr,int vowels,int consonants,int question){
-        if(vowels>=3||consonants>=5)
-            return "UGLY";
-
-        if(itr==s.length()&&(question>=3||test==true))
-            return "42";
-        if(itr==s.length())
-            return "NICE";
-
-        if(s[itr]=='?'){
-            if(vowels==2){
-                if(itr+1<s.length())
-                    test = true;
-                return check(s,itr+1,0,consonants+1,question+1);
-            }if(consonants==4){
-                if(itr+1<s.length())
-                    test = true;
-                return check(s,itr+1,vowels+1,0,question+1);
+        while(itr<=(short)s.length()){
+            if(vowels==3||consonants==5){
+                return "UGLY";
+            }
+            if(itr==s.length()&&(question>=3||test==true)){
+                return "42";
             }
 
-            string checkP = "";
-            string checkN = "";
-            checkP = check(s,itr+1,vowels+1,0,question+1);
-            checkN = check(s,itr+1,0,consonants+1,question+1);
-            if(checkP==checkN&&checkP=="UGLY")
-                return "UGLY";
-            else if(checkP!=checkN||(checkN==checkP&&checkP=="42"))
-                return "42";
-            else
+            if(itr==s.length()){
                 return "NICE";
+            }
+
+            if(s[itr]=='A'||s[itr]=='E'||s[itr]=='I'||s[itr]=='O'||s[itr]=='U'){
+                consonants = 0;
+                question = 0;
+                vowels++;
+            }else if(s[itr]=='?'){
+                if(vowels==2){
+                    if(itr+1<s.length())
+                        test = true;
+                    vowels = 0;
+                    consonants++;
+                }else if(consonants==4){
+                    if(itr+1<s.length())
+                        test = true;
+                    consonants = 0;
+                    vowels++;
+                }else{
+                    string checkP = "";
+                    string checkN = "";
+                    checkP = check(s,itr+1,vowels+1,0,question+1);
+                    checkN = check(s,itr+1,0,consonants+1,question+1);
+                    if(checkP==checkN&&checkP=="UGLY")
+                        return "UGLY";
+                    else if(checkP!=checkN||(checkN==checkP&&checkP=="42"))
+                        return "42";
+                    else
+                        return "NICE";
+                }
+            }else{
+                vowels = 0;
+                question = 0;
+                consonants++;
+            }
+            itr++;
         }
-        else if(s[itr]=='A'||s[itr]=='E'||s[itr]=='I'||s[itr]=='O'||s[itr]=='U')
-            return check(s,itr+1,vowels+1,0,0);
-        else
-            return check(s,itr+1,0,consonants+1,0);
     }
 
 
