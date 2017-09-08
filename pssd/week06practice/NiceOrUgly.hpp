d9daeaf3b834f25b814799f1dddb33f638c5943f
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-08 12:49:40 +0000

    test uglyornice
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@920 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week06practice/NiceOrUgly.hpp
+++ pssd/week06practice/NiceOrUgly.hpp
@@ -24,45 +24,61 @@
 
 using namespace std;
 class NiceOrUgly{
-
-    public:
+public:
     string describe(string s){
+        int check = 0;
         int vowels = 0;
         int consonants = 0;
         int question = 0;
         int itr = 0;
         string res = "NICE";
         while(itr<=(short)s.length()){
-            if(vowels==3||consonants==5)
+            if((vowels==3||consonants==5)&&question==0)
                 return "UGLY";
+            if(vowels+question==3||consonants+question==5)
+                res = "42";
+            if(question>=3)
+                res = "42";
+
             if(itr==s.length())
                 return res;
             if(s[itr]=='A'||s[itr]=='E'||s[itr]=='I'||s[itr]=='O'||s[itr]=='U'){
+                if(check<0){
+                    check = 0;
+                    question = 0;
+                }
+                check++;
                 consonants = 0;
-                question = 0;
                 vowels++;
             }else if(s[itr]=='?'){
                 if(vowels==2){
                     res = "42";
                     vowels = 0;
-                    consonants++;
+                    consonants = 1;
+                    check = -1;
                 }else if(consonants==4){
+                    check = 1;
                     res = "42";
                     consonants = 0;
-                    vowels++;
+                    vowels = 1;
                 }else{
-                    vowels++;
-                    consonants++;
+                    //vowels++;
+                    //consonants++;
                     question++;
                 }
             }else{
+                if(check>0){
+                    check = 0;
+                    question = 0;
+                }
+                check--;
                 vowels = 0;
-                question = 0;
                 consonants++;
             }
             itr++;
         }
     }
+
 
 
 };
