96c8c79258d73c27192c31dbe0c95226ce49273a
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-12 05:54:15 +0000

    test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@934 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/pracexam2p1/KnightsAndField.hpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <ctype.h>
+#include <math.h>
+
+using namespace std;
+
+typedef pair<int,int> ii;
+
+class KnightsAndField{
+	map<ii,int> M;
+
+
+	public:
+		int place(vector<string> place){
+			int res = 0;
+			int n = place.size();
+			int m = place[0].length();
+
+			int count = 0;
+			for(int i=0;i<n;i++){
+				count = 0;
+				for(int j=0;j<m;j++){
+					if(playce[i][j]=='X')
+						count++;
+				}
+
+				if(count==0)
+					res++;
+			}
+
+			return res;
+		}
+
+};
+
+
