0f7f15f4deeccf368ace32ee5365c8b3ba6d84fc
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-12 07:17:30 +0000

    test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@946 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/pracexam2p3/PowerOfTwo.hpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <ctype.h>
+
+using namespace std;
+
+typedef pair<long,long> ii;
+
+class PowerOfTwo{
+	map<long,long> M;
+
+	public:
+		long returnNumber(long N){
+
+			return 1;
+		}
+
+		long help(long remain,long valueN){
+			long res = 0;
+			while(remain!=)
+
+		}
+		
+
+
+
+};
+
+
