959e293daa93f1ae81596ad94ca9476301ac9098
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-20 15:44:13 +0000

    test PrefixFreeSets
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@955 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week07practice/PrefixFreeSets.hpp
+++ pssd/week07practice/PrefixFreeSets.hpp
@@ -21,58 +21,34 @@
 #include <vector>
 
 using namespace std;
-
 class PrefixFreeSets{
-
 public:
     int maxElements(vector<string> words){
-        int res = words.size();
-        vector<string> dup;
-        vector<int> dupPost;
-        vector<bool> checkRemain;
+        vector<string> uniqueSet;
+        uniqueSet.push_back(words[0]);
+        for(int i=1;i<(int)words.size();i++){
+            bool check = true;
+            for(int j=0;j<i;j++){
+                if(words[j]==words[i])
+                    check = false;
+            }
+            if(check)
+                uniqueSet.push_back(words[i]);
+        }
+        int res = uniqueSet.size();
 
-        for(int i=0;i<(int)words.size();i++){
-            string temp = words[i];
-            for(int j=0;j<(int)words.size();j++){
+        for(int i=0;i<(int)uniqueSet.size();i++){
+            string temp = uniqueSet[i];
+            for(int j=0;j<(int)uniqueSet.size();j++){
                 if(i==j)
                     ;
                 else{
-                    if(words[j].length()>temp.length()){
-                        string comp = words[j].substr(0,(int)temp.length());
+                    if(uniqueSet[j].length()>temp.length()){
+                        string comp = uniqueSet[j].substr(0,(int)temp.length());
                         if(temp==comp){
-                            int k = 0;
-                            bool check = true;
-                            for(k=0;k<(int)checkRemain.size();k++){
-                                if(temp==dup[k]){
-                                    if(checkRemain[k])
-                                        checkRemain[k] = false;
-                                    else
-                                        check = false;
-                                    break;
-                                }
-                            }
-
-                            if(check)
-                                res--;
+                            res--;
                             break;
                         }
-                    }else if(words[j]==temp&&i!=j){
-                            bool check = true;
-                            int k = 0;
-                            for(k=0;k<(int)dup.size();k++){
-                                if(words[j]==dup[k]){
-                                    check = false;
-                                    break;
-                                }
-                            }
-
-                            if(check){
-                                res--;
-                                dup.push_back(temp);
-                                dupPost.push_back(i);
-                                checkRemain.push_back(true);
-                            }else if(check==false&&i==dupPost[k])
-                                res--;
                     }
                 }
             }
