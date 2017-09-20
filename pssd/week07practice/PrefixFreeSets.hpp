1625d077c18b0a209dd483be41265c94abb64d6f
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-09-20 14:56:51 +0000

    test PrefixFreeSets
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@954 ebee28f9-b968-474b-8da9-e5d0b78a977b

--- pssd/week07practice/PrefixFreeSets.hpp
+++ pssd/week07practice/PrefixFreeSets.hpp
@@ -21,6 +21,7 @@
 #include <vector>
 
 using namespace std;
+
 class PrefixFreeSets{
 
 public:
@@ -28,6 +29,7 @@ public:
         int res = words.size();
         vector<string> dup;
         vector<int> dupPost;
+        vector<bool> checkRemain;
 
         for(int i=0;i<(int)words.size();i++){
             string temp = words[i];
@@ -38,10 +40,23 @@ public:
                     if(words[j].length()>temp.length()){
                         string comp = words[j].substr(0,(int)temp.length());
                         if(temp==comp){
-                            res--;
+                            int k = 0;
+                            bool check = true;
+                            for(k=0;k<(int)checkRemain.size();k++){
+                                if(temp==dup[k]){
+                                    if(checkRemain[k])
+                                        checkRemain[k] = false;
+                                    else
+                                        check = false;
+                                    break;
+                                }
+                            }
+
+                            if(check)
+                                res--;
                             break;
                         }
-                    }else if(words[j]==temp){
+                    }else if(words[j]==temp&&i!=j){
                             bool check = true;
                             int k = 0;
                             for(k=0;k<(int)dup.size();k++){
@@ -55,7 +70,8 @@ public:
                                 res--;
                                 dup.push_back(temp);
                                 dupPost.push_back(i);
-                            }else if(check==false&&i<=dupPost[k])
+                                checkRemain.push_back(true);
+                            }else if(check==false&&i==dupPost[k])
                                 res--;
                     }
                 }
