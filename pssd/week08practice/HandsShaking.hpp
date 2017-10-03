9560070a1f4f3fa0aa494ff6e58d8dcca6b7b558
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-10-03 09:52:04 +0000

    add handshaking for test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@972 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/week08practice/HandsShaking.hpp
@@ -0,0 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  HandsShaking.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  10/02/2017 03:56:18 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+class HandsShaking{
+
+public:
+    long countPerfect(int n){
+        int tempN = n/2;
+        long res = 0;
+        res = fac(2*tempN)/(fac(tempN+1)*fac(tempN));
+        return res;
+    }
+    long fac(int n){
+        long res = 1;
+        while(n>=1){
+            res = res*n;
+            n--;
+        }
+        return res;
+    }
+
+};
