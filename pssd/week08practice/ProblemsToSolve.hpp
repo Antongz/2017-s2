5f1e0adc6c122fb946f78f00104590270d4e2488
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-10-03 04:05:32 +0000

    add ProblemstoSolve for testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@968 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/week08practice/ProblemsToSolve.hpp
@@ -0,0 +1,58 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ProblemsToSolve.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/27/2017 08:34:51 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+class ProblemsToSolve{
+
+public:
+    int minNumber(vector<int> pleasantness,int variety){
+        int res = 100000;
+
+        for(int i=0;i<(int)pleasantness.size();i++){
+            int temp = pleasantness[i];
+            for(int j=i+1;j<(int)pleasantness.size();j++){
+                if(abs(temp-pleasantness[j])>=variety){
+                    int tempR;
+                    if(i%2==0&&j%2==0){
+                        tempR = (j-i)/2 + 1;
+                    }else if(i%2==0&&j%2!=0){
+                        tempR = (i/2) + 1 + (j-i)/2 + 1;
+                    }else if(i%2!=0&&j%2==0){
+                        tempR = (i/2) + 2 + (j-i)/2 + 1;
+                    }else{
+                        //i%2!=0&&j%2!=0
+                        tempR = (i/2) + 2 + (j-i)/2;
+                    }
+
+                    if(res>tempR)
+                        res = tempR;
+                    break;
+
+                }
+            }
+        }
+        return res;
+    }
+
+};
