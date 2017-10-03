d7ba5d2aeefd7e375374339060ae1a3f609d6c54
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-10-03 03:54:10 +0000

    add Thimbles for testing
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@965 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/week08practice/Thimbles.hpp
@@ -0,0 +1,61 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Thimbles.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/26/2017 07:51:44 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Yi-Ting, Hsieh (a1691807), a1691807@student.adelaide.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+class Thimbles{
+
+public:
+    int thimbleWithBall(vector<string> swaps){
+    //initially the target is in the first one
+        char res = '1';
+        int itr = 0;
+        while(itr<(int)swaps.size()-1){
+            char pos1 = swaps[itr][0];
+            char pos2 = swaps[itr][2];
+            if(res==pos1){
+                res = pos2;
+            }
+            else if(res==pos2){
+                res = pos1;
+            }
+
+            itr++;
+        }
+        int final = 1;
+        switch (res) {
+        case '1':
+            final = 1;
+            break;
+        case '2':
+            final = 2;
+            break;
+        case '3':
+            final = 3;
+            break;
+
+        }
+        return final;
+    }
+
+
+};
