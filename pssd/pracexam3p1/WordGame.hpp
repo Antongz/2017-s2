3a5ce036c5bf2aeee461259c2bb3da5f79fd4e37
a1691807 <a1691807@ebee28f9-b968-474b-8da9-e5d0b78a977b>
2017-10-24 04:55:21 +0000

    test
    git-svn-id: https://version-control.adelaide.edu.au/svn/a1691807/2017/s2@1025 ebee28f9-b968-474b-8da9-e5d0b78a977b

---
+++ pssd/pracexam3p1/WordGame.hpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <ctype.h>
+#include <math.h>
+#include <queue>
+#include <stack>
+
+using namespace std;
+class WordGame{
+
+public:
+	int BestScore(string S){
+		vector<int> count;
+		vector<char> charC;
+
+		for(int i=0;i<(int)S.length();i++){
+
+			bool check = true;
+			for(int j=0;j<(int)charC.size();j++){
+				if(charC[j]==S[i]){
+					check = false;
+					count[j]++;
+					break;
+				}
+
+			}
+			if(check){
+				charC.push_back(S[i]);
+				count.push_back(1);
+			}
+		}
+
+		int res = 0;
+		sort(count.begin(),count.end());
+		reverse(count.begin(),count.end());
+		res = square(count[0]+count[1]);
+		for(int i=2;i<(int)count.size();i++)
+			res += square(count[i]);
+
+
+		return res;
+
+	}
+
+	int square(int x){
+		return x*x;
+	}
+
+
+};
